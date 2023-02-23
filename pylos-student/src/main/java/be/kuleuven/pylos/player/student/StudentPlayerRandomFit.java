621a6075a4e2b8da2e25ee788766e43c8c691226
thomas1901 <49908712+thomas1901@users.noreply.github.com>
2023-02-23 09:49:17 +0100

    Updated StudentPlayerRandomFit

--- pylos-student/src/main/java/be/kuleuven/pylos/player/student/StudentPlayerRandomFit.java
+++ pylos-student/src/main/java/be/kuleuven/pylos/player/student/StudentPlayerRandomFit.java
@@ -17,18 +17,38 @@ public class StudentPlayerRandomFit extends PylosPlayer{
     @Override
     public void doMove(PylosGameIF game, PylosBoard board) {
 		/* add a reserve sphere to a feasible random location */
-
+        ArrayList<PylosLocation> allPossibleLocations = new ArrayList<>();
+        for (PylosLocation bl : board.getLocations()) {
+            if (bl.isUsable()) {
+                allPossibleLocations.add(bl);
+            }
+        }
+        PylosSphere reserveSphere = board.getReserve(this);
+        PylosLocation location = allPossibleLocations.size() == 1 ? allPossibleLocations.get(0) : allPossibleLocations.get(getRandom().nextInt(allPossibleLocations.size() - 1));
+        game.moveSphere(reserveSphere, location);
     }
 
     @Override
     public void doRemove(PylosGameIF game, PylosBoard board) {
 		/* removeSphere a random sphere */
-
+        ArrayList<PylosSphere> removableSpheres = new ArrayList<>();
+        for (PylosSphere ps : board.getSpheres(this)) {
+            if (!ps.isReserve() && !ps.getLocation().hasAbove()) {
+                removableSpheres.add(ps);
+            }
+        }
+        PylosSphere sphereToRemove;
+        if (removableSpheres.size() == 1) {
+            sphereToRemove = removableSpheres.get(0);
+        } else {
+            sphereToRemove = removableSpheres.get(getRandom().nextInt(removableSpheres.size() - 1));
+        }
+        game.removeSphere(sphereToRemove);
     }
 
     @Override
     public void doRemoveOrPass(PylosGameIF game, PylosBoard board) {
 		/* always pass */
-
+        game.pass();
     }
 }
